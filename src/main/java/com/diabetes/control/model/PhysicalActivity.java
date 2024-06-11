2dd4713bb418741c8686c6754af285536817fb5c
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-06-11 10:00:48 +0100

    update

--- src/main/java/com/diabetes/control/model/PhysicalActivity.java
+++
@@ -1,5 +0,0 @@
-package com.diabetes.control.model;
-
-public class PhysicalActivity {
-
-}
--- src/main/java/com/diabetes/control/repository/GlucoseReadingRepository.java
+++ src/main/java/com/diabetes/control/repository/GlucoseReadingRepository.java
@@ -3,5 +3,6 @@ package com.diabetes.control.repository;
 import com.diabetes.control.model.GlucoseReading;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GlucoseReadingRepository extends JpaRepository<GlucoseReading, Long> {
+public interface
+GlucoseReadingRepository extends JpaRepository<GlucoseReading, Long> {
 }
