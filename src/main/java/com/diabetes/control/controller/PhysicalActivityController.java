ed6f851bc38b723f3ff3e258b6919a58e7fe94f0
Nabil Sabi <nabilsabi01@gmail.com>
2024-06-11 10:30:00 +0100

    add repository and service and controller

--- src/main/java/com/diabetes/control/controller/PhysicalActivityController.java
+++ src/main/java/com/diabetes/control/controller/PhysicalActivityController.java
@@ -1,4 +1,38 @@
 package com.diabetes.control.controller;
 
+import com.diabetes.control.model.PhysicalActivity;
+import com.diabetes.control.service.PhysicalActivityService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/activities")
 public class PhysicalActivityController {
+
+    private final PhysicalActivityService physicalActivityService;
+
+    public PhysicalActivityController(PhysicalActivityService physicalActivityService) {
+        this.physicalActivityService = physicalActivityService;
+    }
+
+    @GetMapping
+    public String getAllActivities(Model model) {
+        Long userId = getCurrentUserId(); // Get the current user's ID
+        List<PhysicalActivity> activities = physicalActivityService.findAllByUserId(userId);
+        model.addAttribute("activities", activities);
+        return "physical-activity";
+    }
+
+    @GetMapping("/new")
+    public String showNewActivityForm(Model model) {
+        model.addAttribute("activity", new PhysicalActivity());
+        return "activity-form";
+    }
+
+    private Long getCurrentUserId() {
+        return 1L;
+    }
 }
--- src/main/java/com/diabetes/control/model/PhysicalActivity.java
+++ src/main/java/com/diabetes/control/model/PhysicalActivity.java
@@ -1,5 +1,26 @@
 package com.diabetes.control.model;
 
-public class PhysicalActivity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "physical_activities")
+public class PhysicalActivity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double duration;
+    private LocalDateTime dateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
