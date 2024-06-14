8cfb49ef4f2fed49e0b63c7b5b7ac5502c6c3153
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-06-14 09:34:02 +0100

    update the advice repository

--- src/main/java/com/diabetes/control/repository/AdviceRepository.java
+++ src/main/java/com/diabetes/control/repository/AdviceRepository.java
@@ -1,4 +1,11 @@
 package com.diabetes.control.repository;
 
-public interface AdviceRepository {
+import com.diabetes.control.model.Advice;
+import com.diabetes.control.model.GlucoseLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdviceRepository extends JpaRepository<Advice, Long> {
+    Advice findByGlucoseLevel(GlucoseLevel glucoseLevel);
 }
